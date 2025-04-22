65020a2e258e2fb2af53e20007885f7cc281c1ec
Sujana Gupta <ksujanagupta@gmail.com>
2025-04-23 02:10:11 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./components/SignUp";
-
+import Login from "./components/Login";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
@@ -16,3 +17,4 @@ function App() {
 }
 
 export default App;
+
